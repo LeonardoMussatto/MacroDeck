b8db4e2d24e3e499af1119b1bcd1e27a4f59632e
Leonardo Mussatto <23527613+LeonardoMussatto@users.noreply.github.com>
2022-10-22 15:57:15 +0100

    Reorganise pin defs

--- src/encoders.cpp
+++ src/encoders.cpp
@@ -1,32 +1,6 @@
 // ENHANCE to integrate position and rotation count see https://github.com/siktec-lab/SIK-Rotary-Encoder/blob/master/SIKTEC_Rotary.cpp
 
 #include "encoders.h"
-#include <Adafruit_MCP23X08.h>
-#include <MD_REncoder.h>
-
-#define RE_INT 7
-#define RE_ADDR 0x21
-Adafruit_MCP23X08 re;
-
-#define RE_1A 11
-#define RE_1B 10
-#define RE_2A 13
-#define RE_2B 12
-#define RE_3A 16
-#define RE_3B 17
-#define RE_4A 14
-#define RE_4B 15
-const byte rotaryEncPins[2][4] = {{RE_1A, RE_2A, RE_3A, RE_4A}, {RE_1B, RE_2B, RE_3B, RE_4B}};
-
-#define FAST 100
-MD_REncoder R1 = MD_REncoder(RE_1A, RE_1B);
-MD_REncoder R2 = MD_REncoder(RE_2A, RE_2B);
-MD_REncoder R3 = MD_REncoder(RE_3A, RE_3B);
-MD_REncoder R4 = MD_REncoder(RE_4A, RE_4B);
-
-volatile bool RE_awakenByInterrupt;
-
-//////////////////////////////////////////////////////////////////////////////////////////////////////////
 
 void re_begin()
 {
@@ -56,20 +30,6 @@ void RE_intCallBack()
     RE_awakenByInterrupt = true;
 }
 
-void selectEncoder(Profiles &profile)
-{
-    if (re.getLastInterruptPin() == RE_1A || re.getLastInterruptPin() == RE_1B)
-        handleEncoder(R1.read(), R1.speed(), 0, profile);
-    if (re.getLastInterruptPin() == RE_2A || re.getLastInterruptPin() == RE_2B)
-        handleEncoder(R2.read(), R2.speed(), 1, profile);
-    if (re.getLastInterruptPin() == RE_3A || re.getLastInterruptPin() == RE_3B)
-        handleEncoder(R3.read(), R3.speed(), 2, profile);
-    if (re.getLastInterruptPin() == RE_4A || re.getLastInterruptPin() == RE_4B)
-        handleEncoder(R4.read(), R4.speed(), 3, profile);
-
-    RE_awakenByInterrupt = false;
-}
-
 void handleEncoder(byte dir, uint16_t spd, byte id, Profiles &profile)
 {
     if (spd < FAST)
@@ -86,6 +46,20 @@ void handleEncoder(byte dir, uint16_t spd, byte id, Profiles &profile)
             Keyboard.press(re_macroFast[profile][keyStates[3][id] == 0 ? 1 : 2][id][dir][k]);
         }
     }
+}
+
+void selectEncoder(Profiles &profile)
+{
+    if (re.getLastInterruptPin() == RE_1A || re.getLastInterruptPin() == RE_1B)
+        handleEncoder(R1.read(), R1.speed(), 0, profile);
+    if (re.getLastInterruptPin() == RE_2A || re.getLastInterruptPin() == RE_2B)
+        handleEncoder(R2.read(), R2.speed(), 1, profile);
+    if (re.getLastInterruptPin() == RE_3A || re.getLastInterruptPin() == RE_3B)
+        handleEncoder(R3.read(), R3.speed(), 2, profile);
+    if (re.getLastInterruptPin() == RE_4A || re.getLastInterruptPin() == RE_4B)
+        handleEncoder(R4.read(), R4.speed(), 3, profile);
+
+    RE_awakenByInterrupt = false;
 }
 
 void handleEncoders(Profiles &profile)
--- src/encoders.h
+++ src/encoders.h
@@ -2,6 +2,31 @@
 #define ENCODERS_H
 #include "profiles.h"
 
+#include <Adafruit_MCP23X08.h>
+#include <MD_REncoder.h>
+
+#define RE_INT 7
+#define RE_ADDR 0x21
+Adafruit_MCP23X08 re;
+
+#define RE_1A 11
+#define RE_1B 10
+#define RE_2A 13
+#define RE_2B 12
+#define RE_3A 16
+#define RE_3B 17
+#define RE_4A 14
+#define RE_4B 15
+const byte rotaryEncPins[2][4] = {{RE_1A, RE_2A, RE_3A, RE_4A}, {RE_1B, RE_2B, RE_3B, RE_4B}};
+
+#define FAST 100
+MD_REncoder R1 = MD_REncoder(RE_1A, RE_1B);
+MD_REncoder R2 = MD_REncoder(RE_2A, RE_2B);
+MD_REncoder R3 = MD_REncoder(RE_3A, RE_3B);
+MD_REncoder R4 = MD_REncoder(RE_4A, RE_4B);
+
+volatile bool RE_awakenByInterrupt;
+
 void re_begin();
 void handleEncoders(Profiles &profile);
 
